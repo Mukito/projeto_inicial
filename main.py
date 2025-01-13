6f5732b31a1ea53661bce779642785239e1318a0
Mukito <mukito100@gmail.com>
2025-01-13 13:01:19 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Ola Mundo")
+
+# variaveis
+idade = 45
+nome = "Mukito"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1, 2, 3, 4, 5]
+
+# Métodos
+lista_invertida = lista.reverse()
+
+
+print(lista)
+print("=========")
+print(lista_invertida)
